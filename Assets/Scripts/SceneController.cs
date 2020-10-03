2c214e09df1411f815888842420c767867ea939f
Vladimir <bibubl@yandex.com>
2020-10-04 01:00:43 +0300

    в контроллер сцены добавлен алгоритм перемешивания карт; карты выдаются по 2 одного вида.

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -16,6 +16,10 @@ public class SceneController : MonoBehaviour
     void Start()
     {
         Vector3 startpos = originalCard.transform.position;
+
+        int[] numbers = { 0, 0, 1, 1, 2, 2, 3, 3 };
+        numbers = ShuffleArray(numbers);
+
         for (int i = 0; i < gridCols; i++)
         {
             for (int j = 0; j < gridRows; j++)
@@ -23,7 +27,9 @@ public class SceneController : MonoBehaviour
                 MemoryCard card;
                 if (i == 0 && j == 0) card = originalCard;
                 else card = Instantiate(originalCard);
-                int id = Random.Range(0, images.Length);
+               
+                int index = j * gridCols + i;
+                int id = numbers[index];
                 card.SetCard(id, images[id]);
 
                 float posX = (ofsetX * i) + startpos.x;
@@ -31,10 +37,19 @@ public class SceneController : MonoBehaviour
                 card.transform.position = new Vector3(posX, posY, startpos.z);
             }
         }
-
-        
     }
-
+    int[] ShuffleArray(int[] numbers)
+    {
+        int[] numClones = numbers.Clone() as int[];
+        for (int i = 0; i < numClones.Length; i++)
+        {
+            int tmp = numClones[i];
+            int rndNum = Random.Range(i, numClones.Length);
+            numClones[i] = numClones[rndNum];
+            numClones[rndNum] = tmp;
+        }
+        return numClones;
+    }
     // Update is called once per frame
     void Update()
     {
