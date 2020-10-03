baefcaa51653219337659182b7c05ab9279216e4
Vladimir <bibubl@yandex.com>
2020-10-03 22:16:33 +0300

    Доступный функционал: - карта инстанцируется на 8 рандомных карт; - при нажатии скрывается рубашка карты;

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    [SerializeField] GameObject cardBack = default;
+    [SerializeField] SceneController controller = default;
+
+    int _id;
+    public int Id { get { return _id; } }
+
+    public void OnMouseDown()
+    {
+        if (cardBack.activeSelf) cardBack.SetActive(false);
+    }
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+    public void SetCard(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+   
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] MemoryCard originalCard = default;
+    [SerializeField] Sprite[] images = default;
+
+    public const int gridRows = 2;
+    public const int gridCols = 4;
+    public const float ofsetX = 10f;
+    public const float ofsetY = -10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 startpos = originalCard.transform.position;
+        for (int i = 0; i < gridCols; i++)
+        {
+            for (int j = 0; j < gridRows; j++)
+            {
+                MemoryCard card;
+                if (i == 0 && j == 0) card = originalCard;
+                else card = Instantiate(originalCard);
+                int id = Random.Range(0, images.Length);
+                card.SetCard(id, images[id]);
+
+                float posX = (ofsetX * i) + startpos.x;
+                float posY = (ofsetY * j) + startpos.y;
+                card.transform.position = new Vector3(posX, posY, startpos.z);
+            }
+        }
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
